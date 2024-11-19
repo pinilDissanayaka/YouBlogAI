a18a1ef79000b83d4a8732f5c766358e019058c3
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-11-19 08:23:40 +0530

    add crew ai

--- main.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.document_loaders import YoutubeLoader
-
-
-loader=YoutubeLoader.from_youtube_url("https://www.youtube.com/watch?v=5qtC-tsQ-wE&t=321s", add_video_info=False, language=["en"], translation="en")
-
-data=loader.load()
-
-print(data)
